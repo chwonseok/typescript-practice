61e603b5a2bc30e2041c0a644fb6727584d3260d
cws0316 <dev.chwonseok@gmail.com>
2021-10-26 12:24:51 +0900

    Modify: studied on Narrowing and Assertion

--- index.js
+++ index.js
@@ -2,3 +2,18 @@
 function myFunction(x) {
     return x * 2;
 }
+// Narrowing
+function narrowingFunc(x) {
+    if (typeof x === 'string')
+        return x + '1';
+    else
+        return x + 1;
+}
+narrowingFunc('hey');
+narrowingFunc(27);
+// Assertion
+function assertionFunc(x) {
+    var arr = [];
+    arr[0] = x;
+}
+assertionFunc(123);
--- index.ts
+++ index.ts
@@ -2,3 +2,18 @@
 function myFunction(x: number | undefined) {
   return x * 2;
 }
+
+// Narrowing
+function narrowingFunc(x: number | string) {
+  if (typeof x === 'string') return x + '1';
+  else return x + 1;
+}
+narrowingFunc('hey');
+narrowingFunc(27);
+
+// Assertion
+function assertionFunc(x: number | string) {
+  let arr: number[] = [];
+  arr[0] = x as number;
+}
+assertionFunc(123);
